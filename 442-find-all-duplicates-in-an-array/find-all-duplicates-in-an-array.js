ec5773cec8f2fcc40c191d1652182e9e82002c6c
Sagarsaasy <sinhasagar442@gmail.com>
2025-07-29 00:50:22 +0530

    Time: 28 ms (35.73%) | Memory: 82.2 MB (11.52%) - LeetSync

--- 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.js
+++ 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.js
@@ -7,15 +7,26 @@
 //  arr = [0,1,1,1,0,0,1,1]
 
 var findDuplicates = function(nums) {
-    let arr = [...Array(nums.length)];
-    arr.fill(0);
-    // console.log(arr)
-    result = [];
-    for(let i = 0; i < nums.length; i++) {
-        if(arr[nums[i]-1] !== 1) {
-            arr[nums[i]-1] = 1
-        } else {
+    // let arr = [...Array(nums.length)];
+    // arr.fill(0);
+    // // console.log(arr)
+    // result = [];
+    // for(let i = 0; i < nums.length; i++) {
+    //     if(arr[nums[i]-1] !== 1) {
+    //         arr[nums[i]-1] = 1
+    //     } else {
+    //         result.push(nums[i]);
+    //     }
+    // }
+    // return result;
+
+    let map = new Map();
+    let result = [];
+    for(let i=0; i<nums.length; i++) {
+        if(map.has(nums[i])) {
             result.push(nums[i]);
+        } else {
+            map.set(nums[i], 1);
         }
     }
     return result;
